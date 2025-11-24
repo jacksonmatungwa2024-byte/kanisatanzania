33fffa9d2018c2f75e5ecf7118d0e05f70afdc14
jacksonmatungwa2024-byte <jacksonmatungwa2024@gmail.com>
2025-11-24 18:40:09 +0300

    Update ReportsDashboard.tsx

--- src/app/components/ReportsDashboard.tsx
+++ src/app/components/ReportsDashboard.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 import type { SetActiveTab } from "@/types/tabs";
-import styles from "./Reports.css";
+import styles from "./Reports1.css";
 
 interface ReportsDashboardProps {
   setActiveTab: SetActiveTab;
