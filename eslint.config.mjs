c26d3607536233efc53f51e41418555bb205100f
jacksonmatungwa2024-byte <jacksonmatungwa2024@gmail.com>
2025-11-24 19:14:39 +0300

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,20 +6,26 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
+...compat.extends("next/core-web-vitals", "next/typescript"),
+{
+ignores: [
+"node_modules/**",
+".next/**",
+"out/**",
+"build/**",
+"next-env.d.ts",
+],
+plugins: {
+"@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+},
+rules: {
+"@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+},
+},
 ];
 
 export default eslintConfig;
